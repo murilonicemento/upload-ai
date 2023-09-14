e0f7ade446291edcac9be6b450a1652df44e3c23
murilonicemento <murilo_nascimento22@outlook.com>
2023-09-14 19:38:09 -0300

    criação funcções para conversão de video para audio

--- web/src/components/video-input-form.tsx
+++ web/src/components/video-input-form.tsx
@@ -1,3 +1,5 @@
+import { getFFmpeg } from "@/lib/ffmpeg";
+import { fetchFile } from "@ffmpeg/util";
 import { UploadIcon } from "@radix-ui/react-icons";
 import { FileVideo } from "lucide-react";
 import { useMemo, useRef, useState } from "react";
@@ -18,6 +20,44 @@ export function VideoInputForm() {
     const selectedFile = files[0];
 
     setVideoFile(selectedFile);
+  }
+
+  async function convertVideoToAudio(video: File) {
+    console.log("Convert started");
+
+    const ffmpeg = await getFFmpeg();
+
+    await ffmpeg.writeFile("input.mp4", await fetchFile(video));
+
+    // ffmpeg.on("log", (log) => {
+    //   console.log(log);
+    // });
+
+    ffmpeg.on("progress", (progress) => {
+      console.log("Convert progress: " + Math.round(progress.progress * 100));
+    });
+
+    await ffmpeg.exec([
+      "-i",
+      "inpuy.mp4",
+      "-map",
+      "0:a",
+      "-b:a",
+      "20k",
+      "-acodec",
+      "libmp3lame",
+      "output.mp3",
+    ]);
+
+    const data = await ffmpeg.readFile("output.mp3");
+    const audioFileBlob = new Blob([data], { type: "audio/mpeg" });
+    const audioFile = new File([audioFileBlob], "audio.mp3", {
+      type: "audio/mpeg",
+    });
+
+    console.log("Convert finished");
+
+    return audioFile;
   }
 
   function handleUploadVideo(event: FormEventHandler<HTMLFormElement>) {
