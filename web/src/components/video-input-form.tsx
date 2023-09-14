c889f292165f024d9abbbff3499fe440d2f02084
murilonicemento <murilo_nascimento22@outlook.com>
2023-09-14 18:37:53 -0300

    Criação de componente do formulário do upload do video

---
+++ web/src/components/video-input-form.tsx
@@ -0,0 +1,81 @@
+import { UploadIcon } from "@radix-ui/react-icons";
+import { FileVideo } from "lucide-react";
+import { useMemo, useRef, useState } from "react";
+import { Button } from "./ui/button";
+import { Label } from "./ui/label";
+import { Separator } from "./ui/separator";
+import { Textarea } from "./ui/textarea";
+
+export function VideoInputForm() {
+  const [videoFile, setVideoFile] = useState<File | null>(null);
+  const promptInputRef = useRef<HTMLTextAreaElement>(null);
+
+  function handleFileSelected(event: ChangeEvent<HTMLInputElement>) {
+    const { files } = event.currentTarget;
+
+    if (!files) return;
+
+    const selectedFile = files[0];
+
+    setVideoFile(selectedFile);
+  }
+
+  function handleUploadVideo(event: FormEventHandler<HTMLFormElement>) {
+    event.preventDefault();
+
+    const prompt = promptInputRef.current?.value;
+
+    if (!videoFile) return;
+  }
+
+  const previewURL = useMemo(() => {
+    if (!videoFile) return null;
+
+    return URL.createObjectURL(videoFile);
+  }, [videoFile]);
+  return (
+    <form onSubmit={handleUploadVideo} className="space-y-6">
+      <label
+        htmlFor="video"
+        className="relative border flex w-full rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
+      >
+        {previewURL ? (
+          <video
+            src={previewURL}
+            controls={false}
+            className="pointer-events-none absolute inset-0"
+          />
+        ) : (
+          <>
+            <FileVideo className="w-4 h-4" />
+            Selecione um vídeo
+          </>
+        )}
+      </label>
+      <input
+        type="file"
+        name="video"
+        id="video"
+        accept="video/mp4"
+        className="sr-only"
+        onChange={handleFileSelected}
+      />
+
+      <Separator />
+
+      <div className="space-y-2">
+        <Label htmlFor="transcription_prompt">Prompt de transcrição</Label>
+        <Textarea
+          ref={promptInputRef}
+          id="transcription_prompt"
+          className="leading-relaxed resize-none"
+          placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por vírgula"
+        />
+      </div>
+      <Button type="submit" className="w-full">
+        Carregar vídeo
+        <UploadIcon className="w-4 h-4 ml-2" />
+      </Button>
+    </form>
+  );
+}
