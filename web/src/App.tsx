842bfb08591ffdac3060516bacc2409772ab04e8
murilonicemento <murilo_nascimento22@outlook.com>
2023-09-14 18:40:15 -0300

    inclusão componente do formulário do video

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { FileVideo, Github, Heart, Upload, Wand2 } from "lucide-react";
+import { Github, Heart, Wand2 } from "lucide-react";
 import { Button } from "./components/ui/button";
 import { Label } from "./components/ui/label";
 import {
@@ -11,6 +11,7 @@ import {
 import { Separator } from "./components/ui/separator";
 import { Slider } from "./components/ui/slider";
 import { Textarea } from "./components/ui/textarea";
+import { VideoInputForm } from "./components/video-input-form";
 
 export function App() {
   return (
@@ -52,38 +53,7 @@ export function App() {
         </section>
 
         <aside className="w-80 space-y-6">
-          <form className="space-y-6">
-            <label
-              htmlFor="video"
-              className="border flex w-full rounded-md aspect-video cursor-pointer border-dashed text-sm flex-col gap-2 items-center justify-center text-muted-foreground hover:bg-primary/5"
-            >
-              <FileVideo className="w-4 h-4" />
-              Selecione um vídeo
-            </label>
-            <input
-              type="file"
-              name="video"
-              id="video"
-              accept="video/mp4"
-              className="sr-only"
-            />
-
-            <Separator />
-            <div className="space-y-2">
-              <Label htmlFor="transcription_prompt">
-                Prompt de transcrição
-              </Label>
-              <Textarea
-                id="transcription_prompt"
-                className="leading-relaxed resize-none"
-                placeholder="Inclua palavras-chaves mencionadas no vídeo separadas por vírgula"
-              />
-            </div>
-            <Button type="submit" className="w-full">
-              Carregar vídeo
-              <Upload className="w-4 h-4 ml-2" />
-            </Button>
-          </form>
+          <VideoInputForm />
 
           <Separator />
 
